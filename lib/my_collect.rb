52e1d058f7305ced2d382673627213d49150c163
Parham Javadi <pjfooeve09@yahoo.com>
2020-12-11 10:13:27 -0800

    commit tree 614b41279f8e70a77bc601cbaa71ad4a12aecf56

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect (food)
+    array = []
+    i = 0
+  
+    while i < food.length
+      array << yield(food[i])
+      i += 1
+    end
+      return array
+  end
+  
+  
+  my_collect(["pizza", "pasta"]) { |name| puts "here is your #{name}" } 
 
